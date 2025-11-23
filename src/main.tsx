d86f8d852759b6e06e7606dd408e8edfe878c7be
Adria Hernandez <ahernandezlasc@gmail.com>
2025-11-22 22:17:31 -0600

    utilities file is created and basic proyect files are modified to check components changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import AddRoundKeyOp from './components/AddRoundKeyOp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AddRoundKeyOp state={[[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]} roundKey={[[255, 10, 1, 0], [200, 2, 193, 18], [210, 203, 165, 35], [75, 29, 87, 5]]} stateTitle='Estado' roundKeyTitle='Clave de Ronda' resultTitle='Resultado' />
+
   </StrictMode>,
 )
---
+++ src/utilities/conversor.ts
@@ -0,0 +1,3 @@
+function convertNumber(value: string, fromBase : number, toBase : number){
+    return parseInt(value,fromBase).toString(toBase)
+}// convertNumber
