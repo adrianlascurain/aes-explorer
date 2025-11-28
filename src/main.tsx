4bfcada58d599899c26c7a56c1d69633e91df5db
Adria Hernandez <ahernandezlasc@gmail.com>
2025-11-28 14:12:08 -0600

    Utilities for conversion are updated to support more numeric systems and main script is changed to show first demo of recent components with new parameters

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AddRoundKeyOp from './components/AddRoundKeyOp.tsx'
+import SubBytesOp from './components/SubBytesOp.tsx'
+import AesCipher from "./cipher/AesCipher.ts"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AddRoundKeyOp state={[[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]} roundKey={[[255, 10, 1, 0], [200, 2, 193, 18], [210, 203, 165, 35], [75, 29, 87, 5]]} stateTitle='Estado' roundKeyTitle='Clave de Ronda' resultTitle='Resultado' />
-
+    <AddRoundKeyOp state={[[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]} roundKey={[[255, 10, 1, 0], [200, 2, 193, 18], [210, 203, 165, 35], [75, 29, 87, 5]]} representation='hexPadded' stateTitle='Estado' roundKeyTitle='Clave de Ronda' resultTitle='Resultado' />
+    <SubBytesOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} stateTitle='Estado' resultTitle='Resultado' sBox={AesCipher.invSBox} representation='hex' lowNibbleTitle='Low Nibble: ' highNibbleTitle='High Nibble: ' stateContainerTitle='State Value: '/>
   </StrictMode>,
 )
--- src/utilities/conversor.ts
+++ src/utilities/conversor.ts
@@ -1,3 +1,25 @@
-function convertNumber(value: string, fromBase : number, toBase : number){
-    return parseInt(value,fromBase).toString(toBase)
-}// convertNumber
+import type { numericSystem } from "../types/numericSystems";
+
+export const convert2System = (n: number | null, toSystem:numericSystem) =>{
+    if(n === null){
+        return "";
+    }
+    
+    switch(toSystem){
+        case "bin":
+            return n.toString(2);
+        case "8bitBin":
+            return n.toString(2).padStart(8,'0');
+        case "dec":
+            return n.toString(10);
+        case "invHexPadded":
+            return n.toString(16).padEnd(2,'0');
+        case "hexPadded":
+            return n.toString(16).padStart(2,'0');
+        case "hex":
+            return n.toString(16);
+        default:
+            return n.toString(10);
+    }
+}
+
