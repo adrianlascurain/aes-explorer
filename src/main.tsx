db3aa0da3555fd74555d21002e65bf72ae73d010
Adria Hernandez <ahernandezlasc@gmail.com>
2026-01-12 00:34:02 -0600

    main is updated to show only mix column component

--- src/main.tsx
+++ src/main.tsx
@@ -11,16 +11,17 @@ import PlayGround from './components/PlayGround.tsx';
 import MixColumnsOp from './components/MixColumnsOp.tsx';
 import NavBar from './components/NavBar.tsx';
 import LandingPage from './components/LandingPage.tsx';
+import type { Matrix4x4 } from './types/matrix.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AddRoundKeyOp state={[[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]} roundKey={[[255, 10, 1, 0], [200, 2, 193, 18], [210, 203, 165, 35], [75, 29, 87, 5]]} representation='hexPadded' stateTitle='Estado' roundKeyTitle='Clave de Ronda' resultTitle='Resultado' />
-    {/* <SubBytesOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} stateTitle='Estado' resultTitle='Resultado' sBox={AesCipher.invSBox} representation='hex' lowNibbleTitle='Low Nibble: ' highNibbleTitle='High Nibble: ' stateContainerTitle='State Value: '/> */}
+    {/* <AddRoundKeyOp state={[[0, 4, 8, 12], [1, 5, 9, 13], [2, 6, 10, 14], [3, 7, 11, 15]]} roundKey={[[255, 10, 1, 0], [200, 2, 193, 18], [210, 203, 165, 35], [75, 29, 87, 5]]} representation='hexPadded' stateTitle='State' roundKeyTitle='Round Key' resultTitle='Result' /> */}
+    {/* <SubBytesOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} stateTitle='State' resultTitle='Result' sBox={AesCipher.invSBox} representation='hex' lowNibbleTitle='Low Nibble: ' highNibbleTitle='High Nibble: ' stateContainerTitle='State Value: '/> */}
     
     {/* <ShiftRowsOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} shiftType='normal' representation='dec'/> */}
     {/* <CardCarousel items={CardCarouselLanguage.getCarouselItems('ES')}/> */}
     {/* <PlayGround></PlayGround> */}
-    <MixColumnsOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} />
+    <MixColumnsOp state={[[255, 4, 190, 12], [1, 5, 111, 13], [2, 6, 10, 204], [3, 79, 11, 15]]} fixedPolynomios={AesCipher.MIX_COLUMNS_MATRIX as Matrix4x4}/>
 
     {/* <LandingPage></LandingPage> */}
   </StrictMode>,
