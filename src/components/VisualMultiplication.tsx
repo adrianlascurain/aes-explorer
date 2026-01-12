f376445d543c6c915726da3cb5027254097d32d6
Adria Hernandez <ahernandezlasc@gmail.com>
2026-01-12 00:33:42 -0600

    Visaul multiplication component is refactored to receive single values rather than entire arrays

--- src/components/VisualMultiplication.tsx
+++ src/components/VisualMultiplication.tsx
@@ -1,15 +1,12 @@
-import pentagonIcon from "../assets/pentagon.svg";
-import triangleIcon from "../assets/triangle.svg";
 import multiplicationIcon from "../assets/multiplication.svg";
 import './VisualMultiplication.css'
-import type { Matrix4x4 } from "../types/matrix";
 import type { numericSystem } from "../types/numericSystems";
 import { convert2System } from "../utilities/conversor";
 
-function VisualMultiplication({g1Row,g1Col,g2Row,g2Col,state,representation,g1Img,g2Img} : {g1Row: number,g1Col: number,g2Row: number,g2Col: number, state?: Matrix4x4, representation: numericSystem, g1Img: string, g2Img: string}){
+function VisualMultiplication({g1Row,g1Col,g1Value,g2Row,g2Col,g2Value,representation,g1Img,g2Img} : {g1Row: number,g1Col: number,g1Value: number,g2Row: number,g2Col: number, g2Value:number, representation: numericSystem, g1Img: string, g2Img: string}){
 
-    let g1ValueToShow = representation === "matrixPosition" ? `(${g1Row + 1},${g1Col + 1})` : convert2System(state? state[g1Row][g1Col] : 0,representation);
-    let g2ValueToShow = representation === "matrixPosition" ? `(${g2Row + 1},${g2Col + 1})` : convert2System(state? state[g2Row][g2Col] : 0,representation);
+    let g1ValueToShow = representation === "matrixPosition" ? `(${g1Row + 1},${g1Col + 1})` : convert2System(g1Value? g1Value : 0,representation);
+    let g2ValueToShow = representation === "matrixPosition" ? `(${g2Row + 1},${g2Col + 1})` : convert2System(g2Value? g2Value : 0,representation);
 
     return(
         <div className="vm-multiplication-container">
