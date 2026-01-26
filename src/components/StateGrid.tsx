68c06971d8d898200101a68a59b998ff18e9d13b
Adria Hernandez <ahernandezlasc@gmail.com>
2026-01-26 00:45:48 -0600

    State grid component is updated to add functionality to retrieve cells objects to have more control over styles

--- src/components/StateGrid.tsx
+++ src/components/StateGrid.tsx
@@ -2,6 +2,7 @@ import './StateGrid.css';
 import type { Matrix4x4 } from '../types/matrix';
 import type { numericSystem } from '../types/numericSystems';
 import { convert2System } from '../utilities/conversor';
+import { useImperativeHandle, useRef } from 'react';
 
 type StateGridProps = {
     state: Matrix4x4;
@@ -11,6 +12,14 @@ type StateGridProps = {
     handleOnMouseLeave: (e: React.MouseEvent, i: number, j: number) => void;
     selectedColor: string;
     representation: numericSystem;
+    ref?: React.Ref<StateGridHandle>;
+};
+
+export type StateGridHandle = {
+    getElement: (i: number, j: number) => HTMLElement | null;
+    getAllElements: () => Map<string, HTMLElement>;
+    highlightCell: (i: number, j: number, styles: React.CSSProperties) => void;
+    clearHighlight: (i: number, j: number, styles: React.CSSProperties) => void;
 };
 
 function StateGrid({
@@ -20,16 +29,48 @@ function StateGrid({
     handleOnMouseEnter,
     handleOnMouseLeave,
     selectedColor,
-    representation
+    representation,
+    ref
 }: StateGridProps) {
+    const elementsRef = useRef<Map<string,HTMLElement>>(new Map());
+
+    useImperativeHandle(ref, () => ({
+        getElement: (i: number, j: number) => {
+            return elementsRef.current.get(`${i}-${j}`) || null;
+        },
+        getAllElements: () => {
+            return elementsRef.current;
+        },
+        highlightCell: (i: number, j: number, styles: React.CSSProperties) => {
+            const element = elementsRef.current.get(`${i}-${j}`);
+            if (element) {
+                Object.assign(element.style, styles);
+            }
+        },
+        clearHighlight: (i: number, j: number, styles: React.CSSProperties) => {
+            const element = elementsRef.current.get(`${i}-${j}`);
+            if (element) {
+                Object.assign(element.style, styles);
+            }
+        }
+    }), [])
+
     return (
         <div className="state-wrapper">
             {state.map((row, i) =>
                 row.map((_value, j) => {
                     const selected = isCellSelected(i, j);
+                    const key = `${i}-${j}`;
                     return (
                         <div
-                            key={`${i}-${j}`}
+                            key={key}
+                            ref={(el) => {
+                                if(el) {
+                                    elementsRef.current.set(key,el);
+                                }else {
+                                    elementsRef.current.delete(key)
+                                }
+                            }}
                             className="state-element"
                             style={{ background: selected? selectedColor : '' }}
                             onClick={(e) => handleClickOnElement(e, i, j)}
