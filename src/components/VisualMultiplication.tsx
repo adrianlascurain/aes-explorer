0451c844d86a5960efcbf64af32cafb74d77120e
Adria Hernandez <ahernandezlasc@gmail.com>
2026-04-03 18:17:35 -0600

    VisualMultiplication component is updated to handle when either g1image or g2image is not available so it establishes the display property as block or none depending the case

--- src/components/VisualMultiplication.tsx
+++ src/components/VisualMultiplication.tsx
@@ -3,18 +3,19 @@ import './VisualMultiplication.css'
 import type { numericSystem } from "../types/numericSystems";
 import { convert2System } from "../utilities/conversor";
 
-function VisualMultiplication({g1Row,g1Col,g1Value,g2Row,g2Col,g2Value,representation,g1Img,g2Img} : {g1Row: number,g1Col: number,g1Value: number,g2Row: number,g2Col: number, g2Value:number, representation: numericSystem, g1Img: string, g2Img: string}){
+function VisualMultiplication({g1Row,g1Col,g1Value,g2Row,g2Col,g2Value,representation,g1Img,g2Img} : {g1Row: number,g1Col: number,g1Value: number,g2Row: number,g2Col: number, g2Value:number, representation: numericSystem, g1Img?: string, g2Img?: string}){
 
     let g1ValueToShow = representation === "matrixPosition" ? `(${g1Row + 1},${g1Col + 1})` : convert2System(g1Value? g1Value : 0,representation);
     let g2ValueToShow = representation === "matrixPosition" ? `(${g2Row + 1},${g2Col + 1})` : convert2System(g2Value? g2Value : 0,representation);
-
+    let showG1Img = g1Img && true;
+    let showG2Img = g2Img && true;
     return(
         <div className="vm-multiplication-container">
             <div className="vm-multiplication-grid-container">
-                <figure className="vm-icon-figure">
+                <figure style={showG1Img? {display: "block"}: {display: "none"}} className="vm-icon-figure">
                     <img src={g1Img} alt="" />
                 </figure>
-                <div className="vm-matrix-value-container">
+                <div style={showG1Img? {} : {minWidth: "max-content"}} className="vm-matrix-value-container">
                     {g1ValueToShow}
                 </div>
             </div>
@@ -22,10 +23,10 @@ function VisualMultiplication({g1Row,g1Col,g1Value,g2Row,g2Col,g2Value,represent
                 <img src={multiplicationIcon} alt="" />
             </figure>
             <div className="vm-multiplication-grid-container">
-                <figure className="vm-icon-figure">
+                <figure style={showG2Img? {display: "block"} : {display: "none"}} className="vm-icon-figure">
                     <img src={g2Img} alt="" />
                 </figure>
-                <div className="vm-matrix-value-container">
+                <div style={showG2Img? {} : {minWidth: "max-content"}} className="vm-matrix-value-container">
                     {g2ValueToShow}
                 </div>
             </div>
