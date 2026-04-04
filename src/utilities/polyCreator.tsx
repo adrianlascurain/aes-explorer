55b43b49a7ed361fe29f1d85522e3ccf38f15e96
Adria Hernandez <ahernandezlasc@gmail.com>
2026-04-03 18:32:00 -0600

    PolyCreator is updated to handle cases where the input is an array and not a binary string, and also getPolynomialTerms is updated to consider a new parameter that dictates when a term should have a line-through to indicate xor addition

--- src/utilities/polyCreator.tsx
+++ src/utilities/polyCreator.tsx
@@ -1,24 +1,46 @@
-import type { JSX } from "react";
+import type { CSSProperties, JSX } from "react";
+import { arrayToBinaryString, reducedEquivalent } from "./MixColumnUtils";
 
 // Convert binary string to polynomio
-const getPolynomialTerms = (binary: string) => {
+export const getPolynomialTerms = (binary: string, need2reductStyle?: CSSProperties, replaceWithReduction: boolean = false, setLineThrough?: boolean[]) => {
+    if(!setLineThrough){
+        setLineThrough = new Array(binary.length).fill(false);
+    }
+    
     const terms: JSX.Element[] = [];
     const reversedBinary = binary.split('').reverse(); // Invert string to start from grade 0
-
+    let equivalent: number[] | undefined;
+    let replacements: JSX.Element[];
+    let isLineThrough = false;
     reversedBinary.forEach((bit, index) => {
         if (bit === '1') {
             // Construir el término
             if (index === 0) {
                 // Término constante
-                terms.push(<span key={index}>1</span>);
+                terms.push(<span key={index} style={isLineThrough? {textDecoration: "line-through"}: {textDecoration: "none"}}>1</span>);
             } else if (index === 1) {
                 // Término x
-                terms.push(<span key={index}>x</span>);
+                terms.push(<span key={index} style={isLineThrough? {textDecoration: "line-through"}: {textDecoration: "none"}}>x</span>);
             } else {
                 // Término x^n
+                if(replaceWithReduction && index >= 8){
+                    equivalent = reducedEquivalent.get(index);
+                    if(equivalent){
+                        replacements = getPolynomialTermsByArray(equivalent.reverse(),need2reductStyle,false).reverse();
+                        replacements = replacements.map((term, internalIndex) => (
+                        <span key={`inner-term-${index}-${internalIndex}`}>
+                            {internalIndex > 0 && ' + '}
+                            {term}
+                        </span>
+                    ));
+                    }
+                }
+
                 terms.push(
-                    <span key={index}>
-                        x<sup>{index}</sup>
+                    <span key={index} style={index >= 8 ? need2reductStyle : {}}>
+                        {
+                            replaceWithReduction && index >= 8? <>{replacements}</> : <>x<sup>{index}</sup></>
+                        }
                     </span>
                 );
             }
@@ -28,4 +50,7 @@ const getPolynomialTerms = (binary: string) => {
     return terms;
 };
 
-export default getPolynomialTerms;
+export const getPolynomialTermsByArray = (termsArray: number[], need2reductStyle?: CSSProperties, replaceWithReduction: boolean = false, setLineThrough?: boolean[]) => {
+    const binaryString = arrayToBinaryString(termsArray.reverse());
+    return getPolynomialTerms(binaryString,need2reductStyle,replaceWithReduction,setLineThrough);
+};
