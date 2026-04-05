aa5501821ec270fe19de54d99c2bce2bcf90c55f
Adria Hernandez <ahernandezlasc@gmail.com>
2026-04-05 09:53:21 -0600

    Keys are assigned to polynomios replacements constants

--- src/constants/MixColumnsPolynomialConstants.tsx
+++ src/constants/MixColumnsPolynomialConstants.tsx
@@ -2,7 +2,7 @@ import type { JSX } from "react";
 
 // Reduction for x^10
 const x10: JSX.Element = (
-    <div>
+    <div key={'x10-constant'}>
         <span>x<sup>10</sup></span>
         <span className="mcpc-procedure-arrow">→</span>
         <span>x<sup>2</sup><span className="mcpc-separator">•</span>x<sup>8</sup></span>
@@ -15,7 +15,7 @@ const x10: JSX.Element = (
 
 // Reduction for x^9
 const x9: JSX.Element = (
-    <div>
+    <div key={'x9-constant'}>
         <span>x<sup>9</sup></span>
         <span className="mcpc-procedure-arrow">→</span>
         <span>x<span className="mcpc-separator">•</span>x<sup>8</sup></span>
@@ -28,7 +28,7 @@ const x9: JSX.Element = (
 
 // Reduction for x^8
 const x8: JSX.Element = (
-    <div>
+    <div key={'x8-constant'}>
         <span>x<sup>8</sup></span>
         <span className="mcpc-procedure-arrow">→</span>
         <span> x<sup>4</sup> + x<sup>3</sup> + x + 1</span>
