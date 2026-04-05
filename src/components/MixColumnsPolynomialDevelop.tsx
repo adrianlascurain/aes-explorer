51b065827ec54ff510748ff14f29b4ebc9aed7f7
Adria Hernandez <ahernandezlasc@gmail.com>
2026-04-05 10:06:57 -0600

    MixColumnsPolynomialDevelop component is updated to evaluate when xor operation is needed. HTML is updated too to show the xor addition in the UI

--- src/components/MixColumnsPolynomialDevelop.tsx
+++ src/components/MixColumnsPolynomialDevelop.tsx
@@ -2,15 +2,25 @@ import type {JSX} from "react";
 import { baseReductions } from "../constants/MixColumnsPolynomialConstants";
 import "../constants/MixColumnsPolynomialConstants.css";
 import "./MixColumnsPolynomialDevelop.css";
+import ChainedPolynomialExp from "./ChainedPolynomialExp";
+import { modularGFReduction } from "../utilities/MixColumnUtils";
 
 function MixColumnsPolynomialDevelop({nonReducedCoefficients} : {nonReducedCoefficients: number[]}){
     const polynomiosSubstitutes: Array<JSX.Element> = new Array();
     let needsSubstitution: boolean = false;
+    let needsAddition = false;
     let tempRed;
+    const modularGFReductionArray = modularGFReduction(nonReducedCoefficients); 
     
+    for(let i = 0; i < modularGFReductionArray.length; i++){
+        if(modularGFReductionArray[i] > 1){
+            needsAddition = true;
+            break;
+        }
+    }
+
     if(nonReducedCoefficients.length > 8){
         needsSubstitution = true;
-        console.log(nonReducedCoefficients);
             
         let delta = nonReducedCoefficients.length - 8; // 11 - 8 -> 3
         let initialTerm = nonReducedCoefficients.length - 1; //10
@@ -29,16 +39,23 @@ function MixColumnsPolynomialDevelop({nonReducedCoefficients} : {nonReducedCoeff
     
 
     return (
-        <>
-            <div className="mcpd-polynomial-substitution" style={needsSubstitution? {display:"block"} : {display:"none"}}>
-                <p className="mcpd-polynomial-substitution-description">Substitución</p>
+        <>  
+            <div className="mcpd-polynomial-multiplication" style={needsSubstitution? {display:"block"} : {display:"none"}}>
+                <p className="mcpd-polynomial-multiplication-description">Sustitución</p>
                 <div className="mcpd-polynomial-substitution-content">
                     {polynomiosSubstitutes}
                 </div>
 
+                <div className="mcpd-polynomial-multiplication-substitution-content">
+                    <ChainedPolynomialExp replaceWithReduction nonReducedCoeffcients={nonReducedCoefficients}></ChainedPolynomialExp>
+                </div>
             </div>
-            <div className="mcpd-polynomial-xor-addition">
 
+            <div className="mcpd-polynomial-xor-addition" style={needsAddition? {display:"block"} : {display:"none"}}>
+                <p className="mcpd-polynomial-xor-addition-description">Adicion (XOR)</p>
+                <div className="mcpd-polynomial-xor-addition-content">
+                    <ChainedPolynomialExp applyXOR nonReducedCoeffcients={modularGFReductionArray}></ChainedPolynomialExp>
+                </div>
             </div>
         </>
         
